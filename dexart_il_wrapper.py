18b81c1c0f054033caf55bb6569363b7006144b7
Sriram Krishna <sriramsk1999@gmail.com>
2025-06-28 17:51:35 -0400

    finish eval

--- dexart_il_wrapper.py
+++ dexart_il_wrapper.py
@@ -67,19 +67,49 @@ class DexArt_IL_Wrapper(nn.Module):
         state = torch.cat([obs[key] for key in self.state_keys], dim=-1)
         state = state.to(pcd.device)[:,-1,:]
 
-        pn_feat = self.extractor(pcd)
-        state_feat = self.state_mlp(state)  # B * 64
-        final_feat = torch.cat([pn_feat, state_feat], dim=-1)
-
-        pred, _ = self.mlp_extractor(final_feat) # [policy, value] - discarding value
-
+        pred = self.forward(pcd,  state)
         loss = F.mse_loss(pred, target)
         info = {}
         loss_dict = {'bc_loss': loss.item()}
         return loss, loss_dict, info
 
+    def forward(self, pcd, state):
+        pn_feat = self.extractor(pcd)
+        state_feat = self.state_mlp(state)  # B * 64
+        final_feat = torch.cat([pn_feat, state_feat], dim=-1)
+        pred, _ = self.mlp_extractor(final_feat) # [policy, value] - discarding value
+        return pred
+
     def set_normalizer(self, normalizer: LinearNormalizer):
         self.normalizer.load_state_dict(normalizer.state_dict())
 
-    def predict_action(self, obs):
-        return None
+    def predict_action(self, obs_dict):
+        # incorporate gripper mesh point cloud into the whole point cloud.
+        obs_dict['point_cloud'] = torch.cat([obs_dict['point_cloud'], obs_dict['goal_gripper_pcd']], dim=-2)
+        obs_clean = {k: v for k, v in obs_dict.items() if k not in ['point_cloud', 'imagin_robot', 'goal_gripper_pcd', 'observed_pc_seg-gt', 'imagined_robot_pc_seg-gt']}
+        # normalize input
+        nobs = self.normalizer.normalize(obs_clean)
+        nobs["point_cloud"] = obs_dict["point_cloud"]
+        nobs["imagin_robot"] = obs_dict["imagin_robot"]
+        nobs["goal_gripper_pcd"] = obs_dict["goal_gripper_pcd"]
+        if "observed_pc_seg-gt" in obs_dict:
+            nobs['observed_pc_seg-gt'] = obs_dict['observed_pc_seg-gt']
+            nobs['imagined_robot_pc_seg-gt'] = obs_dict['imagined_robot_pc_seg-gt']
+        this_n_point_cloud = nobs['point_cloud']
+
+        # Only use current observation, action, state
+        pcd = obs_dict["point_cloud"][:,-1,:]
+        state = torch.cat([obs_dict[key] for key in self.state_keys], dim=-1)
+        state = state.to(pcd.device)[:,-1,:]
+
+        # unnormalize prediction
+        naction_pred = self.forward(pcd, state)
+        action_pred = self.normalizer['action'].unnormalize(naction_pred)
+
+        result = {
+            'action': action_pred,
+            'action_pred': {},
+            'info': {},
+        }
+
+        return result
